90a7f1bf99f6c8b2d21129d8b5160095bafeb2ab
Harshal Raverkar <harshal.raverkar@gmail.com>
2021-04-17 15:29:22 +0530

    Strign is palindrome

---
+++ PalindromeString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp210
+{
+  internal static class PalindromeString
+  {
+    public static void Main()
+    {
+      string str = "Malayalama".ToLower();
+      var result = stringPalindrome(str);
+      if (result)
+      {
+        Console.WriteLine("Given string is palindrome");
+      } else
+      {
+        Console.WriteLine("Given string is not palindrome ");
+      }
+    }
+
+    private static bool stringPalindrome(string str)
+    {
+      string res = "";
+      int len = str.Length;
+      if (len % 2 == 0)
+      {
+        return false;
+      } else {
+        for(int i = str.Length-1; i>=0; i--)
+        {
+          res += str[i].ToString();
+        }
+        return str == res;
+      }
+    }
+  }
+}
