258bc9efd01f75a694bf073bdbd7f2b7e062417c
Harshal Raverkar <harshal.raverkar@gmail.com>
2021-04-17 15:03:37 +0530

    string urlify

---
+++ stringurlify.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp210
+{
+  static class stringurlify
+  {
+    public static void Main()
+    {
+      string str = "hello harshal raverkar ".Trim();
+      var t = str.Replace(" ", "%20");
+      Console.WriteLine(t);
+    }
+  }
+}
