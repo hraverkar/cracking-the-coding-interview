707ecc00d94df81c0160dfd6d83b44bf8786a1b1
Harshal Raverkar <harshal.raverkar@gmail.com>
2021-04-18 14:01:58 +0530

    Override example

---
+++ overriding.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp210
+{
+  class base_Class
+  {
+    public virtual void Show()
+    {
+      Console.WriteLine("Hello harshal");
+    }
+  }
+
+  class UpperClass :  base_Class
+  {
+      public  override void Show()
+    {
+      Console.WriteLine("Hello harshal Test");
+    }
+  }
+  internal static class Overriding
+  {
+    public static void Main()
+    {
+      base_Class bc = new base_Class();
+      bc.Show();
+
+      bc = new UpperClass();
+      bc.Show();
+
+    }
+  }
+}
