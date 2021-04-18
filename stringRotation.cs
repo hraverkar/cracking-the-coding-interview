e63ddcb753a35cd3e717d20c72496db9d72d4992
Harshal Raverkar <harshal.raverkar@gmail.com>
2021-04-18 12:53:38 +0530

    string rotation

---
+++ stringRotation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp210
+{
+  class stringRotation
+  {
+    public static void Main()
+    {
+      string str = "helloharshal";
+      stringRotate(str);
+    }
+
+    private static void stringRotate(string str)
+    {
+      string ans = str.Substring(5) + str.Substring(0, 5);
+      Console.WriteLine(ans);
+    }
+  }
+}
