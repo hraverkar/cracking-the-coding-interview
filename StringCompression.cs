3d4b7bd8710f51e44b3fe2cb4e4014a2fde9e347
Harshal Raverkar <harshal.raverkar@gmail.com>
2021-04-18 11:50:27 +0530

    String Compression Program

---
+++ StringCompression.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp210
+{
+  public static class StringCompression
+  {
+    public static void Main()
+    {
+      string str = "aabcccaaa";
+      StringCompresation(str);
+    }
+
+    private static void StringCompresation(string str)
+    {
+      int len = str.Length;
+      var ch = str.ToCharArray();
+      for(int i=0; i < len; i++)
+      {
+        int count = 1;
+        while (i < len - 1 && ch[i] == ch[i + 1])
+        {
+          count++;
+          i++;
+        }
+        Console.Write(ch[i]);
+        Console.Write(count);
+      }
+
+    }
+  }
+}
