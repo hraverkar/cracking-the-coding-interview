9ab0047a2938f9dc64e12c347e24716dd157dee2
Harshal Raverkar <harshal.raverkar@gmail.com>
2021-04-18 13:52:19 +0530

    method overloading

---
+++ Overloading.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp210
+{
+  public class Overloading
+  {
+
+    class Calculator
+    {
+      public int Add(int x, int y)
+      {
+        int p;
+        return p =  x + y;
+      }
+
+      public int Add(int x, int y, int z)
+      {
+        int p;
+        return p = x + y + z;
+      }
+    }
+    public static void Main()
+    {
+      var cal = new Calculator();
+      Console.WriteLine(cal.Add(2, 3));
+      Console.WriteLine(cal.Add(2, 3, 6));
+    }
+  }
+}
